0d8465342e27d1b3212476e02b0a5e5744e54cfe
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-16 14:41:04 +0530

    my code

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,24 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException
+	{
 		
 	// Fill your code
+		Connection con;
+		con=ConnectionManager.getConnection();
+		if(con!=null)
+			System.out.println(" Connection established");
+		else
+			System.out.println("Check your connection");
+		
+		
 		
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,30 @@
 package utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-
+import java.io.InputStream;
 
 public class ConnectionManager {
 	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		//Register the driver class
+		Class.forName("oracle.jdbc.OracleDriver");
+		
+		//create the connection object
+		Connection con = null;
+		con =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Anant007");
+		
+		return con;
+	}
+	
+	public static Properties loadPropertiesFile() throws Exception {
+		Properties prop = new Properties();
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);in.close();
+		return prop; 
+		} 
 	
 }
