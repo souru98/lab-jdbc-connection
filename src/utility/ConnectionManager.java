f441243ef7bec5ecd15657b2c36596d9917f7944
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-16 16:53:24 +0530

    my code

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -10,21 +10,36 @@ import java.io.InputStream;
 public class ConnectionManager {
 	
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		//properties object
+		Properties prop = null;
+		try {
+			prop = loadPropertiesFile();
+		} catch (Exception e1) {
+			
+			e1.printStackTrace();
+		}
+		final String driver = prop.getProperty("driver");
+		final String url = prop.getProperty("url");
+		final String username = prop.getProperty("username");
+		final String password = prop.getProperty("password");
+		
 		//Register the driver class
-		Class.forName("oracle.jdbc.OracleDriver");
+		Class.forName(driver);
 		
 		//create the connection object
 		Connection con = null;
-		con =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Anant007");
-		
+		con =DriverManager.getConnection(url,username,password);
 		return con;
 	}
 	
-	public static Properties loadPropertiesFile() throws Exception {
+	  //properties method
+	  public static Properties loadPropertiesFile() throws Exception 
+	   {
 		Properties prop = new Properties();
 		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
 		prop.load(in);in.close();
 		return prop; 
-		} 
+	   } 
 	
 }
